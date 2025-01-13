66186119568167d3977e5c420b444353e6726efa
Alexander Agnivtsev <agnivtsev@gmail.com>
2025-01-14 00:45:03 +0300

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Alexander Agnivtsev on 14.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
