588c66f8925cf69b7f2963c2a5abc9afcb40b878
Alexander Agnivtsev <agnivtsev@gmail.com>
2025-02-16 22:47:55 +0300

    added messages nd images ranomization

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -43,19 +43,9 @@ struct ContentView: View {
                                 "You Make Me Smile!",
                                 "When the Genius Bar Needs Help. They Call You!"]
                 
-                message = messages[messageNumber]
-                messageNumber += 1
+                message = messages[Int.random(in: 0...messages.count-1)]
                 
-                if messageNumber == messages.count {
-                    messageNumber = 0
-                }
-                
-                imageName = "image\(imageNumber)"
-                imageNumber += 1
-                
-                if imageNumber > 9 {
-                    imageNumber = 0
-                }
+                imageName = "image\(Int.random(in: 0...9))"
                 
             }
             .buttonStyle(.borderedProminent)
